cc9137ed864cbe9d3eb788fcf4d3cf07cf9ed68b
Jakub Danek <danek.ja@gmail.com>
2017-07-04 17:24:20 +0200

    [mavenize] dashboard version 1

---
+++ sources/parkinglot/version1/dashboard/src/main/java/cz/kiv/zcu/relisa/data/parking/dashboard/Dashboard.java
@@ -0,0 +1,54 @@
+package cz.kiv.zcu.relisa.data.parking.dashboard;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import cz.zcu.kiv.relisa.data.parking.lane.statistics.ILaneStatistics;
+import cz.zcu.kiv.relisa.data.parking.statsbase.ICountingStatistics;
+
+public class Dashboard implements Runnable {
+    private static final int NUM_CYCLES = 10;
+    private static final long PAUSE_TIME = 200;
+
+    Logger logger = null;
+    private static final String lid = "Dashboard.r1";
+
+    // dependencies, intentionally generalized type on this endpoint
+    ICountingStatistics gateStats = null;
+    ILaneStatistics laneStats = null;
+
+    public Dashboard(ICountingStatistics gate, ILaneStatistics lane) {
+        this.logger = LoggerFactory.getLogger("parking-demo");
+        logger.info(lid + ": <init>");
+
+        gateStats = gate;
+        gateStats.clear();
+        laneStats = lane;
+        laneStats.clear();
+    }
+
+    @Override
+    public void run() {
+        int gateNum;
+        int laneNum;
+
+        gateNum = gateStats.getEventCount();
+        laneNum = laneStats.getCountVehiclesPassed();
+        logger.info(lid + ": initial stats -- lane passed {}, gate events {}", laneNum, gateNum);
+        for (int i = 0; i < NUM_CYCLES; ++i) {
+            logger.info("*** " + lid + ": loop {}", i);
+            gateNum = gateStats.getEventCount();
+            laneNum = laneStats.getCountVehiclesPassed();
+            logger.info("*** " + lid + " stats: lane passed {}, gate events {}", laneNum, gateNum);
+            try {
+                Thread.sleep(PAUSE_TIME);
+            } catch (InterruptedException e) {
+                logger.warn(lid + ": thread interrupted");
+                e.printStackTrace();
+            }
+            Thread.yield();
+        }
+
+    }
+
+}
---
+++ sources/parkinglot/version1/dashboard/src/main/java/cz/kiv/zcu/relisa/data/parking/dashboard/DashboardActivator.java
@@ -0,0 +1,83 @@
+package cz.kiv.zcu.relisa.data.parking.dashboard;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.BundleException;
+import org.osgi.framework.ServiceReference;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import cz.zcu.kiv.relisa.data.parking.lane.statistics.ILaneStatistics;
+import cz.zcu.kiv.relisa.data.parking.statsbase.ICountingStatistics;
+
+public class DashboardActivator implements BundleActivator {
+
+    private Logger logger;
+    private static final String lid = "Dashboard.r1 Activator";
+
+    private Thread t;
+
+    // service dependencies
+    private ICountingStatistics gateStats = null;    // intentionally using superinterface
+    private ILaneStatistics laneStats = null;
+
+    private Dashboard dashboard;
+
+    public DashboardActivator() {
+        this.logger = LoggerFactory.getLogger("parking-demo");
+    }
+
+    @Override
+    public void start(BundleContext context) throws Exception {
+        logger.info(lid + ": starting");
+
+        // required services
+
+        ServiceReference sr;
+        sr = context.getServiceReference(ICountingStatistics.class.getName());
+        if (sr == null) {
+            logger.error(lid + ": no gate stats registered");
+        } else {
+            gateStats = (ICountingStatistics) context.getService(sr);
+            if (gateStats == null) {
+                logger.error(lid + ": no gate stats service available");
+            } else {
+                logger.info(lid + ": got gate stats");
+            }
+        }
+
+        sr = context.getServiceReference(ILaneStatistics.class.getName());
+        if (sr == null) {
+            logger.error(lid + ": no lane stats registered");
+        } else {
+            laneStats = (ILaneStatistics) context.getService(sr);
+            if (laneStats == null) {
+                logger.error(lid + ": no lane stats service available");
+            } else {
+                logger.info(lid + ": got lane stats");
+            }
+        }
+
+        if (gateStats == null || laneStats == null) {
+            logger.error(lid + ": some service unavailable, exiting");
+            throw new BundleException(lid + ": some service unavailable, exiting");
+        }
+
+        // requirements ok, go ahead
+
+        dashboard = new Dashboard(gateStats, laneStats);
+        t = new Thread(dashboard);
+        logger.info(lid + ": spawning thread");
+        t.start();
+        logger.info(lid + ": thread spawned");
+
+        logger.info(lid + ": started.");
+
+    }
+
+    @Override
+    public void stop(BundleContext context) throws Exception {
+        logger.info(lid + ": stopped.");
+    }
+
+}
